2096cd0d195603904e1f4986232767848b762455
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-25 09:01:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,15 +6,23 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(array)
-  array.each do |planet|
-    puts "#{planet.capitalize}!"
-  end
+def summon_captain_planet(veggie)
+  veggie.map {|x| x.capitalize + "!" }
 end
 
-def long_planeteer_calls(array)
-  
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
-def find_the_cheese
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
