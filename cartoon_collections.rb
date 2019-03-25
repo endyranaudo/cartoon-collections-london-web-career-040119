4ce226fe0c39380c7cf8f85b2bd43fe6b04a0d59
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-25 08:54:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,20 @@
-def roll_call_dwarves(dwarf)
-  dwarf.each_with_index do |name, index|
-    puts "/#{index + 1}.*#{name}/"
-    end
+def roll_call_dwarves(dwarves)
+  i = 1
+  dwarves.each do |dwarf|
+    puts "/#{i}.*#{dwarf}/"
+    i += 1
+  end
 end
 
-def summon_captain_planet(veggie)
-  veggie.map! {|x| x.capitalize + "!" }
+def summon_captain_planet(array)
+  array.each do |planet|
+    puts "#{planet.capitalize}!"
+  end
 end
 
-def long_planeteer_calls(array) 
-  i = 0 
-  if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
-  end 
+def long_planeteer_calls(array)
+  
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
-    cheese_types.include?(type)
-  end 
+def find_the_cheese
 end
-
