351b13c6a19f2fb6429cb1553963043dbb790447
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-25 09:03:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,10 +12,7 @@ end
 
 def long_planeteer_calls(array) 
   i = 0 
-  if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
+  array.any? {|i| i.length > 4}
   i = i + 1
   end 
 end
